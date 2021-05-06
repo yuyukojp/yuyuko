998b2800742980381875d053c387a4c5f5337726
金斗石 <jindoushi@fenrir-tec.com>
2021-05-06 11:05:23 +0800

    直接弹出页面了.完美收工

--- qqhrLive/qqhrLive/Classes/Main/Controller/BaseAnchorViewController.swift
+++ qqhrLive/qqhrLive/Classes/Main/Controller/BaseAnchorViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import AVKit
 
 private let kItemMargin : CGFloat = 10
 let kNormalItemH = kNormalItemW * 3 / 4
 let kPrettyItemH = kNormalItemW * 4 / 3
 private let kHeaderViewH : CGFloat = 50
 let kNormalItemW = (kScreenW - 3 * kItemMargin) / 2
+var playerController = AVPlayerViewController()
 
 private let kNormalCellID = "kNormalCellID"
 let kPrettyCellID = "kPrettyCellID"
@@ -110,7 +112,7 @@ extension BaseAnchorViewController: UICollectionViewDataSource {
 }
 
 //MARK:- 遵守uicollectionview的代理协议
-extension BaseAnchorViewController: UICollectionViewDelegate{
+extension BaseAnchorViewController: UICollectionViewDelegate,AVPlayerViewControllerDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //1.取出主播信息
         let anchor = baseVM.anchorGroups[indexPath.section].anchors[indexPath.item]
@@ -125,7 +127,17 @@ extension BaseAnchorViewController: UICollectionViewDelegate{
     }
     private func pushNormalRoomVc() {
         let normalRoomVc = RoomNormalViewController()
-        navigationController?.pushViewController(normalRoomVc, animated: true)
+       // navigationController?.pushViewController(normalRoomVc, animated: true)
+        guard let url = URL(string: "https://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4") else { return }
+        
+        let player = AVPlayer(url: url)
+        playerController = AVPlayerViewController()
+        playerController.player = player
+        playerController.allowsPictureInPicturePlayback = true
+        playerController.delegate = self
+        playerController.player?.play()
+        
+        self.present(playerController, animated: true, completion: nil)
         
     }
 }
