dc80018a5f01ead008eab6e5fc109a6c63a502c8
yuyukojp <yuyukojp@outlook.jp>
2021-05-09 12:33:20 +0800

    dont know

--- qqhrLive/qqhrLive/Classes/Room/Controller/RoomNormalViewController.swift
+++ qqhrLive/qqhrLive/Classes/Room/Controller/RoomNormalViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import AVKit
 
-class RoomNormalViewController: UIViewController, AVPlayerViewControllerDelegate {
+class RoomNormalViewController: UIViewController, AVPlayerViewControllerDelegate, UIGestureRecognizerDelegate {
     var playerController = AVPlayerViewController()
     
     override func viewDidLoad() {
@@ -27,5 +27,19 @@ class RoomNormalViewController: UIViewController, AVPlayerViewControllerDelegate
         self.present(playerController, animated: true, completion: nil)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        /*//隐藏导航栏
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        //保持收拾
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        */
+    }
+  /*  override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    */
 }
 
